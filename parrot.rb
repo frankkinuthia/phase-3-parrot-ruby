b819940b8558ed14c87824a87daa555273063eb4
Frank <f.kinuthia01@gmail.com>
2023-03-23 05:04:15 +0300

    Test

--- parrot.rb
+++ parrot.rb
@@ -7,3 +7,9 @@ def parrot(string = "Squawk!")
   return string
 end
 
+parrot("Hello world!") # Output: Hello world!  Return Value: "Hello world!"
+parrot() # Output: Squawk!  Return Value: "Squawk!"
+
+
+# If you call the parrot method with an argument, it will output and return that argument. 
+# If you call it without an argument, it will output and return the default value "Squawk!".
